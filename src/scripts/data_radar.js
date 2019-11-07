b7cc9c22b25e271e92622eed6beb19203db73c2e
Jayleen Zhou <zhou.jiayi.1992@gmail.com>
2019-11-06 22:17:02 -0800

    radar

--- src/scripts/data_radar.js
+++ src/scripts/data_radar.js
@@ -1,7 +1,7 @@
 export const occu_data = [{ 
         "year": "1999", 
         "occurances": [{
-            "date": "01-13",
+            "date": "01-13 07:24",
             "time": "07:24",
             "dist": 200,
             "fata": 0,
@@ -12,7 +12,7 @@ export const occu_data = [{
             "engi": 2,
             "reas": "Hit a bird"
         }, {
-            "date": "02-13",
+            "date": "02-13 08:24",
             "time": "08:24",
             "dist": 20,
             "fata": 5,
@@ -23,7 +23,7 @@ export const occu_data = [{
             "engi": 3,
             "reas": "Hit a bird"
         }, {
-            "date": "03-13",
+            "date": "03-17 17:24",
             "time": "17:24",
             "dist": 300,
             "fata": 0,
@@ -34,7 +34,7 @@ export const occu_data = [{
             "engi": 4,
             "reas": "Hit a bird"
         }, {
-            "date": "03-13",
+            "date": "03-13 20:24",
             "time": "20:24",
             "dist": 400,
             "fata": 0,
@@ -45,7 +45,51 @@ export const occu_data = [{
             "engi": 4,
             "reas": "Hit a bird"
         }, {
-            "date": "04-13",
+            "date": "04-13 22:24",
+            "time": "22:24",
+            "dist": 10,
+            "fata": 0,
+            "inju": 1,
+            "phase": 1,
+            "manu": "Boeing",
+            "orga": "Air Canada",
+            "engi": 1,
+            "reas": "Hit a bird"
+        }, {
+            "date": "08-13 20:24",
+            "time": "20:24",
+            "dist": 400,
+            "fata": 0,
+            "inju": 0,
+            "phase": 1,
+            "manu": "Boeing",
+            "orga": "Air France",
+            "engi": 4,
+            "reas": "Hit a bird"
+        }, {
+            "date": "08-13 22:24",
+            "time": "22:24",
+            "dist": 10,
+            "fata": 0,
+            "inju": 1,
+            "phase": 1,
+            "manu": "Boeing",
+            "orga": "Air Canada",
+            "engi": 1,
+            "reas": "Hit a bird"
+        }, {
+            "date": "09-13 20:24",
+            "time": "20:24",
+            "dist": 400,
+            "fata": 0,
+            "inju": 0,
+            "phase": 1,
+            "manu": "Boeing",
+            "orga": "Air France",
+            "engi": 4,
+            "reas": "Hit a bird"
+        }, {
+            "date": "01-13 14:24",
             "time": "22:24",
             "dist": 10,
             "fata": 0,
@@ -57,8 +101,3 @@ export const occu_data = [{
             "reas": "Hit a bird"
         }]
     }, ];
-
-export const test_data = [
-    { "date": "13", "dist": 5.1 },
-    { "date": "14", "dist": 5.8 },
-];
--- src/scripts/occurances.js
+++ src/scripts/occurances.js
@@ -1,5 +1,6 @@
 import * as am4core from "@amcharts/amcharts4/core";
 import * as am4charts from "@amcharts/amcharts4/charts";
+import am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
 import {occu_data, test_data} from "./data_radar";
 
@@ -10,34 +11,102 @@ class Occurances {
         this.container.height = am4core.percent(100);
 
         this.radarChart = undefined;
+        // am4core.useTheme(am4themes_animated);
     }
 
     init(winW, winH) {
-        this.initLineChart();
+        this.initRadarChart();
     }
 
-    initLineChart() {
+    initRadarChart() {
         this.radarChart = this.container.createChild(am4charts.RadarChart);
-
-        var xAxis = this.radarChart.xAxes.push(new am4charts.CategoryAxis());
+        this.radarChart.data = occu_data[0].occurances;
+        let xAxis = this.radarChart.xAxes.push(new am4charts.CategoryAxis());
         xAxis.dataFields.category = "date";
         xAxis.renderer.maxLabelPosition = 0.99;
+        xAxis.renderer.line.disabled = true;
+        xAxis.renderer.labels.template.fillOpacity = 0.4;
+        xAxis.renderer.labels.template.fill = am4core.color("#ffffff");
+        xAxis.renderer.minLabelPosition = 0;
+        xAxis.renderer.minGridDistance = 30;
+        xAxis.renderer.grid.template.location = 0;
+        xAxis.renderer.cellStartLocation = 0.0;
+        xAxis.renderer.cellEndLocation = 0.01;
+        xAxis.renderer.grid.template.stroke = am4core.color("#ffffff");
+        xAxis.tooltip.background.pointerLength = 4;
+        xAxis.tooltip.background.fill = am4core.color("#666666");
+        xAxis.tooltip.background.stroke = xAxis.tooltip.background.fill;
 
-        var yAxis = this.radarChart.yAxes.push(new am4charts.ValueAxis());
+        let yAxis = this.radarChart.yAxes.push(new am4charts.ValueAxis());
         yAxis.renderer.labels.template.verticalCenter = "bottom";
         yAxis.renderer.labels.template.horizontalCenter = "right";
         yAxis.renderer.maxLabelPosition = 0.99;
+        yAxis.min = 0;
+        yAxis.max = 500;
+        yAxis.renderer.grid.template.disabled = true;
+        yAxis.renderer.line.disabled = true;
+        yAxis.tooltip.disabled = true;
+        yAxis.strictMinMax = true;
+        yAxis.renderer.labels.template.fillOpacity = 0;
+        yAxis.renderer.labels.template.fill = am4core.color("#ffffff");
+        yAxis.renderer.grid.template.stroke = am4core.color("#ffffff");
+        yAxis.renderer.inside = true;
+
+
+        let barSeries = this.radarChart.series.push(new am4charts.RadarColumnSeries());
+        barSeries.data = occu_data[0].occurances;
+        barSeries.dataFields.categoryX = "date";
+        barSeries.dataFields.valueY = "dist";
+        barSeries.name = "Distance";
+        barSeries.strokeWidth = 0;
+        barSeries.columns.template.width = am4core.percent(100);
+        barSeries.columns.template.fill = am4core.color("#BB8FCE");
+
+
+        let injuSeries = this.radarChart.series.push(new am4charts.RadarSeries());
+        injuSeries.data = occu_data[0].occurances;
+        injuSeries.dataFields.categoryX = "date";
+        injuSeries.dataFields.valueY = "dist";
+        injuSeries.dataFields.value = "inju";
+        injuSeries.name = "Injuries";
+        injuSeries.strokeOpacity = 0;
+        injuSeries.sequencedInterpolation = true;
+        injuSeries.sequencedInterpolationDelay = 10;
+        // injuSeries.tooltipText = "Injuries: {value}";
+        let injuBullets = injuSeries.bullets.push(new am4charts.CircleBullet());
+        injuBullets.circle.fill = am4core.color("#FFC300");
+        injuBullets.circle.fillOpacity = 0.2;
+        injuBullets.circle.stroke = am4core.color("#FFC300");
+        injuBullets.circle.propertyFields.radius = "inju";
 
         var fataSeries = this.radarChart.series.push(new am4charts.RadarSeries());
         fataSeries.data = occu_data[0].occurances;
         fataSeries.dataFields.categoryX = "date";
         fataSeries.dataFields.valueY = "dist";
-        fataSeries.name = "Fatalities";
-        let bullets = fataSeries.bullets.push(new am4charts.CircleBullet());
-        bullets.circle.fill = am4core.color("#BB8FCE");
-        bullets.circle.stroke = am4core.color("#BB8FCE");
+        fataSeries.dataFields.value = "fata";
+        fataSeries.name = "Distance";
+        fataSeries.strokeOpacity = 0;
         fataSeries.sequencedInterpolation = true;
         fataSeries.sequencedInterpolationDelay = 10;
+        fataSeries.tooltipText = "Fatality: {value}";
+        fataSeries.tooltipText = `[bold]Distance {dist}[/]
+                                ----
+                                Fatalities: {fata}
+                                Injuries: {inju}`;
+        fataSeries.tooltip.pointerOrientation = "vertical";
+        let fataBullets = fataSeries.bullets.push(new am4charts.CircleBullet());
+        fataBullets.circle.fill = am4core.color("#FF5733");
+        fataBullets.circle.fillOpacity = 0.2;
+        fataBullets.circle.stroke = am4core.color("#FF5733");
+        // fataSeries.heatRules.push({
+        //     target: bullets.circle,
+        //     min: 0.5,
+        //     max: 8,
+        //     property: "radius"
+        // });
+        fataBullets.circle.propertyFields.radius = "fata";
+
+        this.radarChart.cursor = new am4charts.RadarCursor();
     }
 
     update() {
